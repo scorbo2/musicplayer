3c50269afc408fda6dee81ec619278d9f4772e81
Steve Corbett <steve@corbett.ca>
2025-12-14 21:21:15 -0700

    Replace cheesy hardcoded space with proper margin code

--- src/main/java/ca/corbett/musicplayer/ui/PlaylistCellRenderer.java
+++ src/main/java/ca/corbett/musicplayer/ui/PlaylistCellRenderer.java
@@ -6,9 +6,19 @@ import ca.corbett.musicplayer.audio.AudioMetadata;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
+import javax.swing.border.EmptyBorder;
 import java.awt.Component;
 
 public class PlaylistCellRenderer extends JLabel implements ListCellRenderer<AudioMetadata> {
+
+    private static final int DEFAULT_MARGIN = 4;
+
+    public PlaylistCellRenderer() {
+        int padding = DEFAULT_MARGIN;
+        setBorder(new EmptyBorder(0, padding, 0, padding));
+        setOpaque(true);
+    }
+
     @Override
     public Component getListCellRendererComponent(JList<? extends AudioMetadata> list, AudioMetadata value, int index, boolean isSelected, boolean cellHasFocus) {
         if (isSelected) {
@@ -23,10 +33,10 @@ public class PlaylistCellRenderer extends JLabel implements ListCellRenderer<Aud
 
         // Defensive: value might be null in some edge cases
         if (value == null) {
-            setText(" (unknown)");
+            setText("(unknown)");
         }
         else {
-            setText(" " + value.getFormatted()); // cheesy - adding a space for padding
+            setText(value.getFormatted());
         }
 
         return this;
